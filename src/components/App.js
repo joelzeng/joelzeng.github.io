1428caf0526e36f89b1e0c31e5b71eda25d8cb99
Joel Zeng <joelzeng@Joels-MBP.lan>
2018-05-01 18:53:22 -0400

    React-js revamp

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,22 @@
 import React, { Component } from 'react';
-import logo from '../assets/images/logo.svg';
 import './App.css';
-// import { Parallax } from 'react-parallax';
-import Particles from 'react-particles-js';
 
-// import Intro from './Intro/Intro';
-import ResumeDownload from './ResumeDownload/ResumeDownload';
-// import Footer from './Footer/Footer'
+import Tittle from './components/Tittle';
+import Panel from './components/DescriptionPanel';
 
 class App extends Component {
+
   render() {
     return (
-      <section className='development'>
-        <header className='development-content'>
-          <img src={logo} className='development-content-logo' alt='logo' />
-          <h1 className='development-content-text'>Under development.</h1>
-          <h3 className='development-content-text'>Moving from Static Html to React js, Come back later.</h3>
-          <ResumeDownload />
-        </header>
-
-        <Particles params={{
-          particles: {
-            number: {value: 200},
-            line_linked: {enable: false}
-          }
-        }} />
-      </section>
+      <div>
+        <div className='rows'>
+          <div className='row'><Tittle /></div>
+          <div className='row'><Panel /></div>
+        </div>
+      </div>
     );
   }
 }
+
 
 export default App;
